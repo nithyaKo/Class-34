99472636f8a38eb998e3aa56b4d177d1a943efcc
nithyaKo <86623961+nithyaKo@users.noreply.github.com>
2021-09-25 13:58:10 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,73 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, box2, box3,box4,box5,box6,box7,box8,box9,box10,box11,box12,box13,box14,box15,box16,box17,box18,box19,box20,box21,box22,box23,box24,box25,box26;
+var hero,monster,rope,ground;
+
+function preload() {
+  bg = loadImage("GamingBackground.png");
+building = loadImage("building copy.jpeg")
+  
+}
+
+function setup() {
+  createCanvas(3000, 800);
+  engine = Engine.create();
+  world = engine.world;
+
+  ground = new Ground(600, 600, 1200, 20);
+
+  hero = new Hero(400,800,250);
+  rope = new Rope(hero.body, { x: 500, y: 50 });
+  monster = new Monster(1100,550,300);
+
+  box1 = new Box(600, 100, 100,100);
+  
+box1 = addImage('building')
+}
+
+function draw() {
+  background(bg);
+  Engine.update(engine);
+  ground.display();
+  box1.display();
+  box2.display();
+  box3.display();
+  box4.display()
+  box5.display()
+  box6.display()
+  box7.display()
+  box8.display()
+  box9.display()
+  box10.display()
+  box11.display()
+  box12.display()
+  box13.display()
+  box14.display()
+  box15.display()
+  box16.display()
+  box17.display()
+  box18.display()
+  box19.display()
+  box20.display()
+  box21.display()
+  box22.display()
+  box23.display()
+  box24.display()
+  box25.display()
+  box26.display()
+
+  hero.display();
+  rope.display();
+  monster.display();
+
+}
+
+function mouseDragged() {
+  Matter.Body.setPosition(hero.body, { x: mouseX, y: mouseY });
+}
+
+
